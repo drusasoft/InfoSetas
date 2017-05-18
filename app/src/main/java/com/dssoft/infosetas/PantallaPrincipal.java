958f10e8ac1c59a85b74f30413209b7b324ff329
drusasoft <drusa.soft@gmail.com>
2017-05-18 15:21:15 +0200

    Esqueleto InfoSetas

--- app/src/main/java/com/dssoft/infosetas/PantallaPrincipal.java
+++ app/src/main/java/com/dssoft/infosetas/PantallaPrincipal.java
@@ -1,13 +1,98 @@
 package com.dssoft.infosetas;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.multidex.MultiDex;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
-public class PantallaPrincipal extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class PantallaPrincipal extends AppCompatActivity
+{
+
+    @BindView(R.id.toolBar_pantalla_principal) Toolbar toolbar;
+    @BindView(R.id.drawerLayout) DrawerLayout drawerLayout;
+    @BindView(R.id.leftDrawer) NavigationView nView;
+
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_principal);
+        MultiDex.install(this);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close)
+        {
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+
+        drawerLayout.addDrawerListener(drawerToggle);
+
     }
+
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();//necesario para que se muestre el icono del NavigationDrawer
+    }
+
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig)
+    {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);//necesario para que se muestre el icono del NavigationDrawer
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+
+        //Necesario para que se abra y cierra le NavigationDrawer al pulsar el boton
+        if(drawerToggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+
+    @OnClick(R.id.button1)
+    public void irPantallaComestibles()
+    {
+        Intent intent = new Intent(this, PantallaSetasComestibles.class);
+        startActivity(intent);
+    }
+
+
 }
